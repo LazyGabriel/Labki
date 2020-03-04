3b3a64a25a9a62cac750ffc10e7ab92dc492a51a
LazyGabriel <ivanlight200@gmail.com>
2020-03-04 21:29:43 +0300

    Optimization of task

--- lab1/src/Tasker.java
+++ lab1/src/Tasker.java
@@ -1,28 +1,17 @@
 public class Tasker {
     public static void task1 (int a, int b) {
-        if (a < b) {
-            while (a <= b) {
+        int cm=min(a,b);
+        int dm=max(a,b);
+            while (cm <= dm) {
                 int d = 2;
-                while ((d * d <= a) && (a % d != 0)) {
+                while ((d * d <= cm) && (cm % d != 0)) {
                     d++;
                 }
-                if ((d * d > a) && (a != 1) && (a > 0)) {
-                    System.out.print (a + " ");
+                if ((d * d > cm) && (cm != 1) && (cm > 0)) {
+                    System.out.print (cm + " ");
                 }
-                a++;
+                cm++;
             }
-        } else {
-            while (b <= a) {
-                int d = 2;
-                while ((d * d <= b) && (b % d != 0)) {
-                    d++;
-                }
-                if ((d * d > b) && (b != 1) && (b > 0)) {
-                    System.out.print (b + " ");
-                }
-                b++;
-            }
-        }
         System.out.println();
     }
 }
